17d83cd5b58adfb52089453c829e16f268957f3d
Antonio Malato <malatoneto@gmail.com>
2021-03-26 16:04:53 -0300

    Resolução

--- exercises/bloco_4/dia_2/script.js
+++ exercises/bloco_4/dia_2/script.js
@@ -1,5 +1,79 @@
-let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
-for (item of names) {
-    console.log(item)
+for (let index = 0; index < numbers.length; index += 1) {
+  console.log(numbers[index]);
 }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    total += numbers[index]
+    console.log(total)
+} */
+
+
+/* let total = 0;
+let media = 0;
+for (let index = 0; index < numbers.length; index += 1) {
+    total += numbers[index]
+}
+
+
+
+console.log(media = (total / numbers.length))
+
+if (media > 20) {
+    (console.log("Valor maior que 20"))
+} else {
+    (console.log("Valor menor que 20"))
+} */
+
+/* let maior = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] > maior) {
+        maior = numbers[index]
+    }
+
+}
+
+console.log(maior)  */
+
+
+/*let odd = 0;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    if (numbers[index] % 2 == 1) {
+        odd += 1
+    }  
+}
+
+if (odd > 0) {
+    console.log(odd)
+} else {
+    console.log("Não tem número impar")
+} */
+/* 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let menor = 1000;
+
+for (let index = 0; index < numbers.length; index += 1) {
+    
+    if (numbers[index] < menor) {
+        menor = numbers[index]
+        
+    }
+
+}
+
+console.log(menor)  */
+
+
+let array = [];
+let valor;
+
+for (let index = 1; index < 25; index += 1) {
+    array.push(index)
+    console.log(array.length / 2)
+}
+
