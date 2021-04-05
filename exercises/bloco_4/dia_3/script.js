06a2f502e18a6e20fcb07ee52332fe671f4765e4
Antonio Malato <malatoneto@gmail.com>
2021-04-05 15:07:37 -0300

    Exercicio

---
+++ exercises/bloco_4/dia_3/script.js
@@ -0,0 +1,37 @@
+/* let n = 5;
+let s = '*'
+let b = '';
+
+
+
+for (let index = 0; index < n; index += 1) {
+    b = b + s;
+
+}
+
+for (let index = 0; index < n; index += 1) {
+    
+    console.log(b)
+}
+ */
+
+
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+  
+};
--- exercises/bloco_4/dia_4/script.js
+++ exercises/bloco_4/dia_4/script.js
@@ -1,23 +1,61 @@
-let car = {
-    type: 'Fiat',
-    model: '500',
-    color: 'white',
+/* let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
 };
+info.recorrente = "Sim";
 
-let player = {
-    name: 'Marta',
-    lastName: 'Silva',
-    age: '34',
-    medal: {
-        golden: 2,
-        silver: 3,
-    }
+let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim',
 }
 
+for (properties in info) {
 
-player.bestInWorld = [2006, 2007, 2008, 2009, 2018]
+   console.log(info[properties] + ' e ' + info2[properties])
+}
+ */
+/* let array1 = [2, 3, 6, 7, 10, 16, 0, 2, 50];
 
+function maiorIndice(numeros) {
+    let indiceMaior = 0;
 
-console.log("A jogadora " + player.name + ' ' + player.lastName + ' foi eleita a melhor do mundo 6 vezes')
+    for (let indice in numeros) {
+        if (numeros[indiceMaior] < numeros[indice]) {
+            indiceMaior = indice
+        }
+    }
+    return indiceMaior
 
-console.log("A jogadora possui " + player.medal.golden + " de ouro e " + player.medal.silver + ' medalhas de prata' )
+}
+
+console.log(maiorIndice(array1))
+
+function menorIndice(numeros) {
+    let menorValor = 0;
+    for (menor in numeros) {
+        if (numeros[menorValor] > numeros[menor]) {
+            menorValor = menor
+            console.log(menor)
+        }
+    }
+    return menorValor
+}
+console.log(menorIndice(array1))
+
+*/
+let arrayString = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+ 
+function maiorCaracteres(nomes) {
+    let maiorNome = "";
+    for (nome in nomes) {
+        if (nomes[maiorNome] < nomes[nome]) {
+            maiorNome = nomes[nome]
+        }
+    }
+    return maiorNome
+}
+
+console.log(maiorCaracteres(arrayString));
