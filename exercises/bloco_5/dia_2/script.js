1c1bcb5b312eb987dcf10086901e7ef75e76033c
Antonio Malato <malatoneto@gmail.com>
2021-04-07 00:05:39 -0300

    Desafio 2 e 3

--- exercises/bloco_5/dia_2/script.js
+++ exercises/bloco_5/dia_2/script.js
@@ -1,6 +1,5 @@
-/* Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
-Adicione a tag div com a classe main-content como filho da tag body ;
-Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+/*
+
 Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
@@ -10,8 +9,14 @@ Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
  */
 let body = document.getElementsByTagName('body')[0];
 let h1 = document.createElement('h1');
-h1.innerText = 'Exercício 5.2 - JavaScript';
+h1.innerText = 'Exercício 5.2 - JavaScript DOM como filho da tag body';
 body.appendChild(h1)
 
-
+let div = document.createElement('div');
+div.className = 'main-content';
+body.appendChild(div)
 console.log(h1)
+
+let div2 = document.createElement('div');
+div2.className = 'center-content';
+body.appendChild(div2)
