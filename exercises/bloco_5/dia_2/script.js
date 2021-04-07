7847d24cc0c7fbc4dc061681fe3b3ed9d302b90b
Antonio Malato <malatoneto@gmail.com>
2021-04-07 00:12:51 -0300

    Desafio 5

--- exercises/bloco_5/dia_2/script.js
+++ exercises/bloco_5/dia_2/script.js
@@ -1,15 +1,15 @@
 /*
-Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
-Adicione a tag div com a classe main-content como filho da tag body ;
-Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+1 Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+2 Adicione a tag div com a classe main-content como filho da tag body ;
+3 Adicione a tag div com a classe center-content como filho da tag div criada no passo 2;
+4 Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 
-Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
+5 Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 
-Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
-Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
-Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
-Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
-Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
+6 Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
+7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
+8 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
+9 Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
 
  */
 let body = document.getElementsByTagName('body')[0];
@@ -29,3 +29,8 @@ div.appendChild(div2)
 let p = document.createElement('p');
 p.innerText = 'Programação na Trybe';
 div2.appendChild(p);
+
+let div3 = document.createElement('div');
+div3.className = 'left-content';
+
+div.appendChild(div3)
