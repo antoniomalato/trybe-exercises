af843da53599a38f7ad8b0620ae706cc7989e868
Antonio Malato <malatoneto@gmail.com>
2021-04-07 00:41:46 -0300

    Desafio 8

--- exercises/bloco_5/dia_2/script.js
+++ exercises/bloco_5/dia_2/script.js
@@ -5,10 +5,10 @@
 4 Adicione a tag p como filho do div criado no passo 3 e coloque algum texto;
 5 Adicione a tag div com a classe left-content como filho da tag div criada no passo 2;
 6 Adicione a tag div com a classe right-content como filho da tag div criada no passo 2;
-
 7 Adicione uma imagem com src configurado para o valor https://picsum.photos/200 e classe small-image . Esse elemento deve ser filho do div criado no passo 5;
 
 8 Adicione uma lista não ordenada com os valores de 1 a 10 por extenso como valores da lista. Essa lista deve ser filha do div criado no passo 6;
+
 9 Adicione 3 tags h3 , todas sendo filhas do div criado no passo 2.
 
  */
@@ -22,7 +22,6 @@ body.appendChild(h1)
 let div = document.createElement('div');
 div.className = 'main-content';
 body.appendChild(div)
-console.log(h1)
 
 //passo 3
 let div2 = document.createElement('div');
@@ -51,3 +50,12 @@ image.className = 'small-image';
 div3.appendChild(image)
 
 //passo 8 
+let lista = document.createElement('ul');
+
+for (let index = 0; index < 10; index += 1) {
+    let item = document.createElement('li');
+    item.innerText = index;
+    lista.appendChild(item)
+}
+
+div4.appendChild(lista);
