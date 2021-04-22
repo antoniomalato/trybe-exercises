618a7114b500b0c284c00912ebc6122561418c6e
Antonio Malato <malatoneto@gmail.com>
2021-04-22 15:28:31 -0300

    Exercicios forms

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -1,3 +1,16 @@
 const state = document.getElementById('input-state');
-const itemState = document.createElement('div');
 
+function createState() {
+  const stateOptions = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+  for (let index = 0; index < stateOptions.length; index += 1) {
+    const createOptions = document.createElement('option');
+    state.appendChild(createOptions).innerText = stateOptions[index];
+    state.appendChild(createOptions).value = stateOptions[index];
+  }
+}
+
+createState();
+
+function validateData(data) {
+  
+}
