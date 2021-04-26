ed98c361015e63c71b778ec66208072f38a5696a
Antonio Malato <malatoneto@gmail.com>
2021-04-26 18:45:22 -0300

    Exercicio 2

--- exercises/bloco_7/dia_1/script.js
+++ exercises/bloco_7/dia_1/script.js
@@ -11,3 +11,23 @@ const testingScope = escopo => {
 }
 
 testingScope(true);
+
+//Exercício 2
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const number = () => {
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13
+
+  return oddsAndEvens
+}
+const sort = number();
+
+console.log(`Os números ${sort} se encontram de forma crescente`)
+
+
+console.log(oddsAndEvens);
