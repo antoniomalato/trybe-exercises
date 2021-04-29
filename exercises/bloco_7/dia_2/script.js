2c8af6e383ed91f580ae3661112512e205a4bc21
Antonio Malato <malatoneto@gmail.com>
2021-04-29 10:03:07 -0300

    Exercicio 1 parte II

--- exercises/bloco_7/dia_2/script.js
+++ exercises/bloco_7/dia_2/script.js
@@ -38,7 +38,6 @@ const customerInfo = (order) => {
   return `Olá, ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R: ${order.address.street}, N ${order.address.number}, AP: ${order.address.apartment}`
 }
 console.log(customerInfo(order));
-/* Complete a função orderModifier() para que seu retorno seja similar a "Olá Luiz Silva, o total do seu pedido de muzzarella, calabresa e Coca-Cola Zero é R$ 50,00." */
 
 const orderModifier = (order) => {
   // Adicione abaixo as informações necessárias.
@@ -52,3 +51,42 @@ const orderModifier = (order) => {
   return `Olá ${person}, o total do seu pedido de ${flavor1} e ${flavor2} e ${soda} é ${pay}`
 }
 console.log(orderModifier(order));
+
+//Parte II - Exercício 1
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+const addItem = (obj, key, value) => {
+  obj[key] = value
+}
+
+console.log(addItem(lesson2, 'turno', 'noite'))
+
+
+
+
+/*
+1-Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+2-Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
+3-Crie uma função para mostrar o tamanho de um objeto.
+4-Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
+5-Crie um objeto de nome allLessons , que deve agrupar todas as aulas através do Object.assign . 6-Cada chave desse novo objeto será uma aula, sendo essas chaves: lesson1 , lesson2 e lesson3 . Ao executar o comando console.log(allLessons) , a saída deverá ser a seguinte:
+*/
