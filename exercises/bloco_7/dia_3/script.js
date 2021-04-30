369a810feff42828cbe4e06783a203fb81a4f05c
Antonio Malato <malatoneto@gmail.com>
2021-04-30 14:51:03 -0300

    Exercicios teste

--- exercises/bloco_7/dia_3/script.js
+++ exercises/bloco_7/dia_3/script.js
@@ -1,9 +1,12 @@
 const assert = require('assert');
 
 function division(x, y) {
+  if (y === 0) throw new Error('parameter y must not be 0')
   return x / y;
 }
 
-const expected = division(9, 3);
 
-assert.strictEqual(expected, '3', 'Nove dividido por três é igual a três');
+/* assert.strictEqual(division(10, 2), 5); // OK */
+assert.strictEqual(division(10,2), 5);
+
+assert.throws(() => {division(10, 0); /^Error: parameter y must not be 0$/})
